ffb871bfc6f00237d7efc45c99f56413122ffd00
Leonid Artemev <leonidart14@gmail.com>
2023-03-28 02:04:40 +0700

    exps sstream

--- experiments/str_stream/main.cpp
+++ experiments/str_stream/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
-using namespace std;
+#include <sstream>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::stringstream sin{ "@startuml basic    string" };
+    std::cout << "sin.str: " << sin.str() << std::endl;
+
+    std::string word;
+
+    sin >> word;
+    std::cout << word << std::endl;
+
+    sin >> word;
+    std::cout << word << std::endl;
+
+    char ch{ 0 };
+    sin >> ch;
+    std::cout << ch << std::endl;
+    sin.unget();
+
+    sin >> word;
+    std::cout << word << std::endl;
+
+    std::cout << "sin.str: " << sin.str() << std::endl;
+
     return 0;
 }
